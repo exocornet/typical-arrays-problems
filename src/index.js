e88ca064f902b634436e400f07fd90f9e61130cc
Evgeniy Sevostyanov <exocornet@gmail.com>
2020-10-03 12:30:56 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 
 exports.min = function min (array) {
-  return 0;
+
+
+    if (array === undefined || array.length === 0) {
+        return 0;
+
+    } else {
+        let min = array[0];
+        for (let i = 0; i <= array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    if (array === undefined || array.length === 0) {
+        return 0;
+
+    } else {
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+
+    } else {
+        let avg = array[0];
+        let x = 0;
+        for (let i = 0; i < array.length; i++) {
+            x = x + array[i];
+        }
+        avg = x / array.length;
+        return avg;
+    }
 }
