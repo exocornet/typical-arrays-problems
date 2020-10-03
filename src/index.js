6f04a660f1e466ab650068feab0757eb1dec6516
Evgeniy Sevostyanov <exocornet@gmail.com>
2020-10-03 12:36:31 +0300

    fix: avg

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ exports.max = function max (array) {
 
     } else {
         let max = array[0];
-        for (let i = 0; i < array.length; i++) {
+        for (let i = 0; i <= array.length; i++) {
             if (max < array[i]) {
                 max = array[i];
             }
